f0d6c979b67a3239a8f837d57d25f415e7a4d8fe
BrianRobert44 <robert442004@gmail.com>
2026-02-03 10:53:27 +0530

    intial

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ShieldCheck, UserCheck, FileSearch, Clock, ArrowRight } from "lucide-react";
-import {Button} from "../Components/ui/Button.jsx";
+import {Button} from "./ui/Button";
 import heroimage from "../assets/logo/Heroimage.png";
 
 export default function Hero() {
